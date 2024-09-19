f0bfba41ab7a12202428922b455d14b73ab1ab92
Han Fangyuan <hanfangyuan_ustb@163.com>
2024-09-19 22:58:39 +0800

    auto_coder_132_chat_action.yml_f214a7ce7cc6e7946b07d1737c4511fb

--- channel/gewechat/gewechat_message.py
+++ channel/gewechat/gewechat_message.py
@@ -6,5 +6,25 @@ from common.log import logger
 class GeWeChatMessage(ChatMessage):
     def __init__(self, gewechat_msg, is_group=False):
         super().__init__(gewechat_msg)
-        # Initialize the message attributes
-        pass
+        self.msg_id = gewechat_msg["MsgId"]
+        self.create_time = gewechat_msg["CreateTime"]
+        self.is_group = is_group
+
+        if gewechat_msg["Type"] == TEXT:
+            self.ctype = ContextType.TEXT
+            self.content = gewechat_msg["Text"]
+        elif gewechat_msg["Type"] == VOICE:
+            self.ctype = ContextType.VOICE
+            self.content = TmpDir().path() + gewechat_msg["FileName"]
+            self._prepare_fn = lambda: gewechat_msg.download(self.content)
+        elif gewechat_msg["Type"] == PICTURE:
+            self.ctype = ContextType.IMAGE
+            self.content = TmpDir().path() + gewechat_msg["FileName"]
+            self._prepare_fn = lambda: gewechat_msg.download(self.content)
+        else:
+            raise NotImplementedError("Unsupported message type: {}".format(gewechat_msg["Type"]))
+
+        self.from_user_id = gewechat_msg["FromUserName"]
+        self.to_user_id = gewechat_msg["ToUserName"]
+
+        # Additional attribute initializations as needed

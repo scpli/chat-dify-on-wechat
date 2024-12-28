e26009733185d11dead207ae14bb8f88f3efe160
Han Fangyuan <hanfangyuan_ustb@163.com>
2024-12-28 17:49:58 +0800

    fix: return false when itchat wxsid error

--- lib/itchat/components/login.py
+++ lib/itchat/components/login.py
@@ -205,6 +205,8 @@ def process_login_info(core, loginContent):
         core.loginInfo['skey'] = core.loginInfo['BaseRequest']['Skey'] = skey
     if "wxsid" not in cookies:
         logger.error("\n[itchat] 请检查是否对微信实名认证！！！\n")
+        core.isLogging = False
+        return False
     core.loginInfo['wxsid'] = core.loginInfo['BaseRequest']['Sid'] = cookies["wxsid"]
     core.loginInfo['wxuin'] = core.loginInfo['BaseRequest']['Uin'] = cookies["wxuin"]
     if pass_ticket is not None:
